c60b2187e0e38ad837d51941e4e8fe1cef07c6c9
You <You@example.com>
2021-03-06 01:04:32 +0300

    make demo for 1-st lab

---
+++ Lecture1/Work1Task1Num9/Work1Task1Num9.cpp
@@ -0,0 +1,42 @@
+﻿// Work1Task1Num9.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    int G = 0; // birht year
+    int currentYear = 0;
+    std::cout << "enter G: ";
+    std::cin >> G;
+    std::cout << "enter current year: ";
+    std::cin >> currentYear;
+    int age = currentYear - G;
+    if (age >= 11 && age <= 14)
+    {
+        std::cout << age << " let";
+        
+    }
+    else
+    {
+        switch (age % 10)
+        {
+            case 1:
+                std::cout << age << " god";
+                break;
+            case 2:
+            case 3:
+            case 4:
+                std::cout << age << " goda";
+                break;
+            case 0:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+                std::cout << age << " let";
+                break;
+        }
+    }
+}
---
+++ Lecture1/Work1Task2Num9/Work1Task2Num9.cpp
@@ -0,0 +1,43 @@
+﻿// Work1Task2Num9.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <math.h>
+
+int main()
+{
+    int phiVariant = 0;
+    double z = 0;
+    std::cout << "enter phi variant (1, 2 or 3): ";
+    std::cin >> phiVariant;
+    std::cout << "enter x: ";
+    std::cin >> z;
+
+    double x = 0;
+    if (z <= 0)
+        x = z * z / 2;
+    else
+        x = sqrt(z);
+
+    double phi = 0;
+    switch (phiVariant)
+    {
+        case 1:
+            phi = 2 * x;
+            break;
+        case 2:
+            phi = x * x;
+            break;
+        case 3:
+            phi = x / 3.;
+            break;
+        default:
+            std::cout << "go fuck yourself";
+            return -1;
+    }
+
+    double a = 2;
+    double b = 3;
+    double y = (b * phi / cos(x)) + a * log(abs(tan(x / 2)));
+    std::cout << "y is " << y << std::endl;
+}
